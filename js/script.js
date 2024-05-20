9bddd96d8cf7f4284ae53030289fd6dff4fe9827
Abeer <abeer-mostafa@outlook.com>
2024-05-20 19:00:46 +0300

    done the swipe down

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
+// ARROW
+var arrow = document.querySelectorAll('.arrow');
+console.log('just loaded again')
+
+for(let i = 0; i < arrow.length; i++) {
+  console.log('just loaded 3rd time ')
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) { 
+      arrow[i].style.opacity = '0';
+      console.log('scroll has happened')
+  
+    } else if (window.scrollY === 0) {
+      arrow[i].style.opacity = '0.7';
+
+  
+    } else {
+      arrow[i].style.opacity = '0.7';
+
+    }
+  });
+  
+}
+console.log('just loaded 4th')
+
+// slide on first and last slides 
 carousel.onSwipe = function(event) {
   var currentItemIndex = carousel.getCurrentItemIndex();
 
   if (currentItemIndex === 0) {
+    console.log('just loaded 5th')
+
     event.stopPropagation(); // Prevents the swipe action from sliding backwards
     // or
     return false; // Alternative way to prevent the swipe action
@@ -16,13 +44,3 @@ carousel.onSwipe = function(event) {
   }
 };
 
-// ARROW
-var arrows = document.querySelectorAll('.arrow');
-
-document.addEventListener('scroll', handleScroll);
-
-function handleScroll() {
-  arrows.forEach(function(arrow) {
-    arrow.setAttribute("hidden", "");
-  });
-}
